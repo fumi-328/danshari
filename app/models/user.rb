492c0136095b87cc6b946ae868b3eb87f1321cfa
fumi-328 <galaxias.f328@gmail.com>
2024-02-09 17:45:43 +0900

    AuthenticationとUserの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[line]
+
+  has_one :authentication
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_09_055243) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_09_084157) do
+  create_table "authentications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "email"
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_09_055243) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "authentications", "users"
 end
