6a20bb50dc4253676abfc60f44102927e62eb924
fumi-328 <galaxias.f328@gmail.com>
2024-02-11 00:38:24 +0900

    new createの作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'static_pages#top'
 
-  resources :posts, only: %i[index]
+  resources :posts, only: %i[index new create]
 end
