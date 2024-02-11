cc6275c21790f937874e329f2930bb037fac5f5c
fumi-328 <galaxias.f328@gmail.com>
2024-02-11 20:05:49 +0900

    フォームの編集

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'static_pages#top'
 
-  resources :posts, only: %i[index new create edit update destroy]
+  resources :posts
 end
