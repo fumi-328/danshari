cd2590ebc21b4831ed5676b6c004352f03ec7276
fumi-328 <galaxias.f328@gmail.com>
2024-02-15 17:17:15 +0900

    ログアウト関連

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Users::SessionsController < Devise::SessionsController
+    def destroy
+        super do
+          flash.clear
+          flash[:success] = t('defaults.flash_message.logout')
+          redirect_to root_path and return
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    sessions: 'users/sessions',
     omniauth_callbacks: "omniauth_callbacks"
   }
 
