f3aa5ca301e9f1e60c0582ca3f1a71e54c3d2416
fumi-328 <galaxias.f328@gmail.com>
2024-02-15 12:02:58 +0900

    mypagesの編集

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,4 +1,8 @@
 class MypagesController < ApplicationController
+    before_action :authenticate_user!
+
     def show
+      @q = current_user.posts.ransack(params[:q]) # ransackを使用している場合
+      @posts = @q.result(distinct: true).page(params[:page]).order("created_at desc")
     end
 end
