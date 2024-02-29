9271107d2764742d2e1e4e5856b22d99056b4ab7
fumi-328 <galaxias.f328@gmail.com>
2024-02-29 18:59:46 +0900

    postのshowページをログイン状態のみに

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :authenticate_user!, only: %i[new create edit update destroy show]
 
   def index
     @q = Post.ransack(params[:q])
