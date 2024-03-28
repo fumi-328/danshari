873630b5c8a7c9a884a497e063004093f6fde442
fumi-328 <galaxias.f328@gmail.com>
2024-03-28 18:32:00 +0900

    プレビュー実装(検証)

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,47 +1,39 @@
-document.addEventListener('DOMContentLoaded', function(){
-  console.log("DOM fully loaded and parsed");
+document.addEventListener('turbolinks:load', function(){
+  console.log("DOM fully loaded and parsed with Turbolinks");
   
   const postForm = document.getElementById('new_post');
   console.log("postForm: ", postForm);
-  
+
   const previewList = document.getElementById('previews');
   console.log("previewList: ", previewList);
-  
+
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
 
   const fileField = document.querySelector('input[type="file"][name="post[post_image]"]');
-  console.log("fileField: ", fileField);
-  
   if (!fileField){
     console.log("fileField is not found.");
     return;
   }
+  console.log("fileField: ", fileField);
 
   fileField.addEventListener('change', function(e){
-    console.log("File input changed");
-    
     const alreadyPreview = document.querySelector('.preview');
-    console.log("alreadyPreview: ", alreadyPreview);
-    
     if (alreadyPreview) {
       alreadyPreview.remove();
       console.log("Removed existing preview");
-    };
-    
+    }
+
     const file = e.target.files[0];
-    console.log("Selected file: ", file);
-    
     const blob = window.URL.createObjectURL(file);
-    console.log("Blob URL: ", blob);
-    
+
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
-    
+
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
-    
+
     previewWrapper.appendChild(previewImage);
     previewList.appendChild(previewWrapper);
 
