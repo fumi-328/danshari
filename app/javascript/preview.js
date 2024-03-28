22dc7f59a345c221dd275f6679fad46a03c6f7bf
fumi-328 <galaxias.f328@gmail.com>
2024-03-28 17:08:15 +0900

    プレビュー実装(検証)

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,34 +1,49 @@
 document.addEventListener('DOMContentLoaded', function(){
-  // 新規投稿・編集ページのフォームを取得
+  console.log("DOM fully loaded and parsed");
+  
   const postForm = document.getElementById('new_post');
-  // プレビューを表示するためのスペースを取得
+  console.log("postForm: ", postForm);
+  
   const previewList = document.getElementById('previews');
-  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  console.log("previewList: ", previewList);
+  
   if (!postForm) return null;
 
-  // input要素を取得。name情報は検証ツールから確認すること！！
   const fileField = document.querySelector('input[type="file"][name="post[post_image]"]');
-  // input要素で値の変化が起きた際に呼び出される関数
+  console.log("fileField: ", fileField);
+  
+  if (!fileField){
+    console.log("fileField is not found.");
+    return;
+  }
+
   fileField.addEventListener('change', function(e){
-  // 古いプレビューが存在する場合は削除(最後に記述する)
-  const alreadyPreview = document.querySelector('.preview');
-  if (alreadyPreview) {
-    alreadyPreview.remove();
-  };
-  // 取得した画像ファイルの情報は、変数fileに格納する
-  const file = e.target.files[0];
-  // 取得した画像情報のURLを生成(生成されたURLはblobという変数に代入)
-  const blob = window.URL.createObjectURL(file);
-  // 画像を表示するためのdiv要素を生成
-  const previewWrapper = document.createElement('div');
-  previewWrapper.setAttribute('class', 'preview');
-  // 表示する画像を生成
-  const previewImage = document.createElement('img');
-  previewImage.setAttribute('class', 'preview-image');
-  // setAttributeメソッドを用いて生成したimg要素のsrc属性へ変数blobを設定する
-  previewImage.setAttribute('src', blob);
-  // 生成したHTMLの要素をブラウザに表示させる
-  previewWrapper.appendChild(previewImage);
-  previewList.appendChild(previewWrapper);
+    console.log("File input changed");
+    
+    const alreadyPreview = document.querySelector('.preview');
+    console.log("alreadyPreview: ", alreadyPreview);
+    
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+      console.log("Removed existing preview");
+    };
+    
+    const file = e.target.files[0];
+    console.log("Selected file: ", file);
+    
+    const blob = window.URL.createObjectURL(file);
+    console.log("Blob URL: ", blob);
+    
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+    
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+
+    console.log("Preview appended");
   });
 });
