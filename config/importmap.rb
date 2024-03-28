7802ac2d31c9e04b055f0171021e023c535d4261
fumi-328 <galaxias.f328@gmail.com>
2024-03-28 16:33:15 +0900

    プレビュー実装の下準備

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap' # @5.3.2
 pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js'
+pin "preview", to: "preview.js"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,3 +17,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile << 'bootstrap.min.js'
 Rails.application.config.assets.precompile += %w[*.woff *.woff2]
+Rails.application.config.assets.precompile += ['*.js','*.css']
